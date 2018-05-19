89a0150ba65a3fb0e04d1c0db8b1428592cd9674
najar1412 <rory.jarrel@gmail.com>
2018-05-18 22:48:43 -0400

    directional light as a sun

--- relatedapp/static/js/FirstPersonControls.js
+++ relatedapp/static/js/FirstPersonControls.js
@@ -157,6 +157,8 @@ THREE.FirstPersonControls = function ( object, domElement ) {
 			case 82: /*R*/ this.moveUp = true; break;
 			case 70: /*F*/ this.moveDown = true; break;
 
+			case 81: /*Q*/ this.activeLook = false; break;
+
 		}
 
 	};
@@ -179,6 +181,8 @@ THREE.FirstPersonControls = function ( object, domElement ) {
 
 			case 82: /*R*/ this.moveUp = false; break;
 			case 70: /*F*/ this.moveDown = false; break;
+
+			case 81: /*Q*/ this.activeLook = true; break;
 
 		}
 
