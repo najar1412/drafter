55e26c92617209b6386605397a712c63390957f1
najar1412 <rory.jarrel@gmail.com>
2018-05-22 22:10:15 -0400

    clean up

--- visualengine/polls/views.py
+++ visualengine/polls/views.py
@@ -8,6 +8,11 @@ from django.utils import timezone
 from .models import Question
 
 
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
