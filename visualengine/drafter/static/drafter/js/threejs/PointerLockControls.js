073197ca2ae3ed2f21721777e894b7e76951d7e2
najar1412 <rory.jarrel@gmail.com>
2018-06-16 15:48:05 -0400

    basic imple of camera swapping

--- visualengine/drafter/static/drafter/js/threejs/PointerLockControls.js
+++ visualengine/drafter/static/drafter/js/threejs/PointerLockControls.js
@@ -9,10 +9,15 @@ THREE.PointerLockControls = function ( camera ) {
 	camera.rotation.set( 0, 0, 0 );
 
 	var pitchObject = new THREE.Object3D();
+	pitchObject.position.set(camera.position.x, camera.position.y, camera.position.z );
 	pitchObject.add( camera );
 
 	var yawObject = new THREE.Object3D();
-	yawObject.position.y = 10;
+	console.log("^^^^^^^^^^^^^^^^^^^^^^^^^^");
+	console.log(camera.position);
+	console.log(yawObject);
+	yawObject.position.set(camera.position.x, camera.position.y, camera.position.z );
+	console.log(yawObject);
 	yawObject.add( pitchObject );
 
 	var PI_2 = Math.PI / 2;
